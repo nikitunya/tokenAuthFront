ab52154ef0d82d8e94ec84217a54741a2654c78a
nikitunya <nikitagorcakov14@gmail.com>
2025-10-28 20:18:34 +0200

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+}
